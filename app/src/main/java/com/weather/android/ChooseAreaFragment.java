f632404a2901b92ea89dcd359c3ec448a570534a
wyh <playmusichappy@126.com>
2017-03-23 11:38:30 +0800

    完成三级列表 省 市 县

---
+++ app/src/main/java/com/weather/android/ChooseAreaFragment.java
@@ -0,0 +1,225 @@
+package com.weather.android;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.weather.android.db.City;
+import com.weather.android.db.County;
+import com.weather.android.db.Province;
+import com.weather.android.util.HttpUtil;
+import com.weather.android.util.Utility;
+
+import org.litepal.crud.DataSupport;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
+
+/**
+ * Created by wyh on 2017/3/23.
+ */
+
+public class ChooseAreaFragment extends Fragment {
+    public static final int LEVE_PROVINCE = 0;
+    public static final int LEVE_CITY = 1;
+    public static final int LEVE_COUNTY = 2;
+    private ProgressDialog progressDialog;
+    private TextView titleText;
+    private Button backButton;
+    private ListView listView;
+    private ArrayAdapter<String> adapter;
+    private List<String> datalist = new ArrayList<>();
+    private List<Province> provinceList;
+    private List<City> cityList;
+    private List<County> countyList;
+    private Province selectProvince;
+    private City selectCity;
+    /**
+     * 当前选中的级别
+     */
+    private int currentLevel;
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (currentLevel == LEVE_PROVINCE) {
+                    selectProvince = provinceList.get(position);
+                    queryCities();
+                } else if (currentLevel == LEVE_CITY) {
+                    selectCity = cityList.get(position);
+                    queryCounties();
+                }
+            }
+        });
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (currentLevel == LEVE_COUNTY) {
+                    queryCities();
+                } else if (currentLevel == LEVE_CITY) {
+                    queryProvinces();
+                }
+            }
+        });
+        queryProvinces();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.choose_area, container, false);
+        titleText = (TextView) view.findViewById(R.id.title_text);
+        backButton = (Button) view.findViewById(R.id.back_button);
+        listView = (ListView) view.findViewById(R.id.list_view);
+        adapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, datalist);
+        listView.setAdapter(adapter);
+        return view;
+    }
+
+    /**
+     * 查询所有的省份信息，先在数据库查询，没有再到网络查询
+     */
+    private void queryProvinces() {
+        titleText.setText("中国");
+        backButton.setVisibility(View.GONE);
+        provinceList = DataSupport.findAll(Province.class);
+        if (provinceList.size() > 0) {
+            datalist.clear();
+            for (Province province : provinceList) {
+                datalist.add(province.getProvinceName());
+            }
+            adapter.notifyDataSetChanged();
+            listView.setSelection(0);
+            currentLevel = LEVE_PROVINCE;
+        } else {
+            String address = "http://www.guolin.tech/api/china";
+            queryFromServer(address, "province");
+        }
+    }
+
+    private void queryCities() {
+        titleText.setText(selectProvince.getProvinceName());
+        backButton.setVisibility(View.VISIBLE);
+        cityList=DataSupport.where("provinceid=?",String.valueOf(selectProvince.getId())).find(City.class);
+        if (cityList.size()>0){
+            datalist.clear();
+            for (City city:cityList){
+                datalist.add(city.getCityName());
+            }
+            adapter.notifyDataSetChanged();
+            listView.setSelection(0);
+            currentLevel=LEVE_CITY;
+        }else {
+            int provinceCode=selectProvince.getProvinceCode();
+            String address = "http://www.guolin.tech/api/china"+"/"+provinceCode;
+            queryFromServer(address,"city");
+        }
+
+    }
+
+    private void queryCounties() {
+        titleText.setText(selectCity.getCityName());
+        backButton.setVisibility(View.VISIBLE);
+        countyList=DataSupport.where("cityid=?",String.valueOf(selectCity.getId())).find(County.class);
+        if (countyList.size()>0){
+            datalist.clear();
+            for (County county:countyList){
+                datalist.add(county.getCountyName());
+            }
+            adapter.notifyDataSetChanged();
+            listView.setSelection(0);
+            currentLevel=LEVE_COUNTY;
+        }else {
+            int provinceCode=selectProvince.getProvinceCode();
+            int cityCode=selectCity.getCityCode();
+            String address = "http://www.guolin.tech/api/china"+"/"+provinceCode+"/"+cityCode;
+            queryFromServer(address,"county");
+        }
+    }
+
+    private void queryFromServer(String address, final String type) {
+        showProgressDialog();
+        HttpUtil.sendOKHttpRequest(address, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                //通过runOnUiThread()方法回到主线程处理逻辑
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        closeProgressDialog();
+                        Toast.makeText(getContext(), "数据加载失败", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String responsetext = response.body().string();
+                boolean result = false;
+                if ("province".equals(type)) {
+                    result = Utility.handleProvinceResponse(responsetext);
+                } else if ("city".equals(type)) {
+                    result = Utility.handleCityResponse(responsetext, selectProvince.getId());
+                } else if ("county".equals(type)) {
+                    result = Utility.handleCountyResponse(responsetext, selectCity.getId());
+                }
+                if (result) {
+                    getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            closeProgressDialog();
+                            if ("province".equals(type)) {
+                                queryProvinces();
+                            } else if ("city".equals(type)) {
+                                queryCities();
+                            } else if ("county".equals(type)) {
+                                queryCounties();
+                            }
+
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    /**
+     * 显示进度条
+     */
+    private void showProgressDialog() {
+        if (progressDialog == null) {
+            progressDialog = new ProgressDialog(getActivity());
+            progressDialog.setMessage("正在加载。。。");
+            progressDialog.setCanceledOnTouchOutside(false);
+
+        }
+        progressDialog.show();
+    }
+
+    /**
+     * 关闭进度对话框
+     */
+    private void closeProgressDialog() {
+        if (progressDialog != null) {
+            progressDialog.dismiss();
+        }
+    }
+}
